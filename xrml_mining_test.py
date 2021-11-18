4602af4a2d8e13f03eee92722fc1f73531b5afa8
Hitomi Hatsukaze <hitomikannagi@yahoo.co.jp>
2021-11-18 23:40:29 +0900

    Modify: word extraction

--- xrml_mining_test.py
+++ xrml_mining_test.py
@@ -1,5 +1,6 @@
 import re
 from lxml import etree
+from janome.tokenizer import Tokenizer
 
 filename = "./XBRL/PublicDoc/jpcrp040300-q2r-001_E07801-000_2021-09-30_01_2021-11-15.xbrl"
 
@@ -12,4 +13,11 @@ description = re.sub('<.*?>','',description)
 
 print(description)
 
+t = Tokenizer()
+words = []
+for token in t.tokenize(description):
+    if token.part_of_speech.split(',')[0] in ['形容詞', '動詞', '名詞']:
+        words.append(token.base_form)
 
+print(words)
+    

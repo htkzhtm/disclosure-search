a631c0577792fb4f8e5b8140e8a0fd7788ba482b
Hitomi Hatsukaze <hitomikannagi@yahoo.co.jp>
2021-11-19 11:02:05 +0900

    Modify: date specify commandline. (This specification will be aboloshed.)

--- test.py
+++ test.py
@@ -1,16 +1,23 @@
+import sys
 import requests
 import urllib3
 from urllib3.exceptions import InsecureRequestWarning
 urllib3.disable_warnings(InsecureRequestWarning)
 
-url = "https://disclosure.edinet-fsa.go.jp/api/v1/documents.json"
+# Specify the date. Not rigorously checked, because this limitation will be abolished.
+if len(sys.argv) < 2:
+  print("Specify the date(YYYY-MM-DD) command line arg.")
+  sys.exit()
 
-params = {
-  "date" : "2021-11-11",
-  "type" : 2
-}
-
-res = requests.get(url, params=params, verify=False)
-
-print(res.text)
-
+# Get the Disclosures submitted on YYYY-MM-DD
+jsonData = requests.get(
+  url = "https://disclosure.edinet-fsa.go.jp/api/v1/documents.json",
+  params = {
+    "date" : sys.argv[1],
+    "type" : 2
+  },
+  verify=False
+).json()
+if (jsonData["metadata"]["message"] == "404"):
+  print ("Maybe illegal date. Check your insert date. Or, disclosure submitted this date may not be existed.")
+  sys.exit()

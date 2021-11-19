f91418212de4950c37a0f3e87510138ed405ad6d
Hitomi Hatsukaze <hitomikannagi@yahoo.co.jp>
2021-11-19 19:16:16 +0900

    Add: stock code generator for particular disclosure

--- disclosureModule/stockCodeGuarantee.py
+++ disclosureModule/stockCodeGuarantee.py
@@ -12,3 +12,10 @@ class stockCodeClass:
             int(stockCode) > 1000 and
             int(stockCode) < 9999
         )
+
+    # stock code generation for disclosure search.
+    # require: stockCode: string
+    # return: string
+    def genarateStockCode (self, stockCode):
+        return stockCode + "0"
+        

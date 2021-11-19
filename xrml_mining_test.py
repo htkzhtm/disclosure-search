df67da2654c8de9208d51b7242c2ee6063c2d64f
Hitomi Hatsukaze <hitomikannagi@yahoo.co.jp>
2021-11-19 11:08:26 +0900

    WIP: add the wordCloud Generation. This specification may be withdrawn.

--- xrml_mining_test.py
+++ xrml_mining_test.py
@@ -1,17 +1,18 @@
 import re
 from lxml import etree
 from janome.tokenizer import Tokenizer
+from wordcloud import WordCloud
 
-filename = "./XBRL/PublicDoc/jpcrp040300-q2r-001_E07801-000_2021-09-30_01_2021-11-15.xbrl"
+filename = "./XBRL/PublicDoc/jpcrp030000-asr-001_E02367-000_2021-03-31_01_2021-06-30.xbrl"
 
 head = etree.parse(filename).getroot()
 
 company = head.find("jpcrp_cor:CompanyNameCoverPage",head.nsmap).text
-description = head.find("jpcrp_cor:DescriptionOfBusinessTextBlock",head.nsmap).text
+description = head.find("jpcrp_cor:BusinessPolicyBusinessEnvironmentIssuesToAddressEtcTextBlock",head.nsmap).text
 description = re.sub('\s','',description)
 description = re.sub('<.*?>','',description)
 
-print(description)
+#print(description)
 
 t = Tokenizer()
 words = []
@@ -19,5 +20,12 @@ for token in t.tokenize(description):
     if token.part_of_speech.split(',')[0] in ['形容詞', '動詞', '名詞']:
         words.append(token.base_form)
 
-print(words)
-    
+#print(words)
+
+wordcloud = WordCloud(
+    background_color='white', 
+    font_path = '/System/Library/Fonts/ヒラギノ角ゴシック W2.ttc', 
+    stopwords = ['株式会社', '当社', '事業','関連','内容','セグメント','とおり', '現在', '会社','関係']
+)
+wordcloud.generate(' '.join(words))
+wordcloud.to_file('wordcloud.png')
